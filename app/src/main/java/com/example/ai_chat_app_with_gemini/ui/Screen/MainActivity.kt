5d5a1dbb112d0435276dce5553de9645a94b32d6
Jilu Jose <jilujose@Jilus-MacBook-Pro.local>
2024-01-27 23:27:20 -0800

    adding minor UI Update

--- app/src/main/java/com/example/ai_chat_app_with_gemini/ui/Screen/MainActivity.kt
+++ app/src/main/java/com/example/ai_chat_app_with_gemini/ui/Screen/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.ai_chat_app_with_gemini.ui.Screen
-import coil.compose.rememberAsyncImagePainter
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
@@ -31,6 +30,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AddPhotoAlternate
 import androidx.compose.material.icons.rounded.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -43,6 +43,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -53,6 +54,7 @@ import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
 import com.example.ai_chat_app_with_gemini.R
@@ -86,8 +88,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                      color = MaterialTheme.colorScheme.background
                 ) {
-//                    ChatBgScreen(Modifier.fillMaxSize())
-                    Scaffold(
+                     Scaffold(
                         topBar = {
                             Box(
                                 modifier = Modifier
@@ -111,33 +112,13 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-//    @Composable
-//    fun ChatBgScreen(modifier: Modifier = Modifier) {
-//        Scaffold(
-//            topBar = {
-//                Box(modifier = modifier
-//                    .fillMaxWidth()
-//                    .background(MaterialTheme.colorScheme.primary)
-//                    .height(55.dp)
-//                    .padding(horizontal = 16.dp)
-//                ){
-//                    Text(
-//                        modifier = Modifier.align(Alignment.Center),
-//                        text = stringResource(id = R.string.app_name),
-//                        fontSize = 19.sp,
-//                        color = MaterialTheme.colorScheme.onPrimary)
-//                }
-//            }
-//        ) {
-//            ChatScreen(it)
-//        }
-//    }
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun ChatScreen1(paddingValues: PaddingValues) {
+    fun ChatScreen(paddingValues: PaddingValues) {
         val chaViewModel = viewModel<ChatViewModel>()
         val chatState = chaViewModel.chatState.collectAsState().value
 
-        val bitmap = getBitMap()
+        val bitmap = getBitmap()
 
         Column(
             modifier = Modifier
@@ -233,101 +214,6 @@ class MainActivity : ComponentActivity() {
         }
 
     }
-
-    @Composable
-    private fun ChatScreen(paddingValue : PaddingValues){
-        val chatViewModel = viewModel<ChatViewModel>()
-        val chatState = chatViewModel.chatState.collectAsState().value
-
-        Column (modifier = Modifier
-            .fillMaxSize()
-            .padding(top = paddingValue.calculateTopPadding()),
-            verticalArrangement = Arrangement.Bottom
-        ){
-
-            LazyColumn(modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp),
-                reverseLayout = true
-            ){
-                itemsIndexed(chatState.chatList){
-                        index, chat ->
-                    if (chat.isFromUser){
-                        UserChatItem(prompt = chat.prompt, bitmap = chat.bitmap)
-                    }else{
-                        ModelChatItem(response = chat.prompt)
-                    }
-                }
-            }
-
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                Column {
-                    chatState.bitmap?.let {
-                        Image(contentDescription = "Pick Image",
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(40.dp)
-                                .padding(bottom = 2.dp)
-                                .clip(RoundedCornerShape(6.dp)),
-                            bitmap = it.asImageBitmap()
-                        )
-                    }
-                    
-                    Icon(
-                        modifier = Modifier
-                            .size(40.dp)
-                            .clickable {
-                                imagePicker.launch(
-                                    PickVisualMediaRequest
-                                        .Builder()
-                                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                                        .build()
-                                )
-                            },
-                        imageVector = Icons.Rounded.AddPhotoAlternate, contentDescription = "Add photo",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                }
-                    
-                    Spacer(modifier = Modifier.width(8.dp))
-
-                    TextField(
-                        value = chatState.prompt,
-                        onValueChange = {
-                        chatViewModel.onEvent(ChatUIEvent.UpdatePrompt(it))
-                        },
-                        placeholder = {Text(text = "Type prompt")}
-                        ,
-                        modifier = Modifier
-                            .weight(1f)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Icon(
-                        modifier = Modifier
-                            .size(40.dp)
-                            .clickable {
-                                chatViewModel.onEvent(
-                                    ChatUIEvent.SendPrompt(
-                                        chatState.prompt,
-                                        chatState.bitmap
-                                    )
-                                )
-                                uriState.update { "" }
-                            },
-                        imageVector = Icons.Rounded.Send, contentDescription = "Add photo",
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-            }
-        }
-    }
-
     @Composable
     private fun UserChatItem(prompt : String,bitmap: Bitmap?){
 
@@ -348,7 +234,8 @@ class MainActivity : ComponentActivity() {
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(12.dp))
                 .background(MaterialTheme.colorScheme.primary)
-                .padding(16.dp), fontSize = 17.sp, color = MaterialTheme.colorScheme.onPrimary
+                .padding(16.dp), fontSize = 17.sp,
+                color = MaterialTheme.colorScheme.onPrimary
 
             )
         }
@@ -356,34 +243,33 @@ class MainActivity : ComponentActivity() {
     @Composable
     private fun ModelChatItem(response : String){
 
-        Column(modifier = Modifier.padding(start = 100.dp, bottom = 22.dp)) {
+        Column(modifier =
+        Modifier.padding(end = 100.dp, bottom = 22.dp)) {
             Text(text = response, modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(12.dp))
                 .background(MaterialTheme.colorScheme.tertiary)
-                .padding(16.dp), fontSize = 17.sp, color = MaterialTheme.colorScheme.onTertiary
-
+                .padding(16.dp), fontSize = 17.sp,
+                color = MaterialTheme.colorScheme.onTertiary
             )
         }
     }
-
     @Composable
-        private fun getBitMap() : Bitmap?{
-
+    private fun getBitmap(): Bitmap? {
         val uri = uriState.collectAsState().value
 
-        val imageState : AsyncImagePainter.State = rememberAsyncImagePainter(
+        val imageState: AsyncImagePainter.State = rememberAsyncImagePainter(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(uri)
                 .size(Size.ORIGINAL)
                 .build()
         ).state
 
-        if (imageState is AsyncImagePainter.State.Success){
+        if (imageState is AsyncImagePainter.State.Success) {
             return imageState.result.drawable.toBitmap()
         }
 
-            return null
+        return null
     }
 
 }
